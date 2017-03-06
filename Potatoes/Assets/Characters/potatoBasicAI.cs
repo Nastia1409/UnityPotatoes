fab51a23873927c39eb61d4e92ff19d4fac9e932
Nastia1409 <anastasia.koifman@gmail.com>
2017-03-06 22:20:11 +0200

    add Mouse basic actions

--- Potatoes/Assets/Characters/potatoBasicAI.cs
+++ Potatoes/Assets/Characters/potatoBasicAI.cs
@@ -2,40 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
+using System;
+using Assets.Scripts.Interfaces;
 
-public class potatoBasicAI : MonoBehaviour {
-    
-
+public class PotatoBasicAI : MonoBehaviour, IPotatoAI
+{
     public Transform followTarget;
 
     private bool moving = true;
     // Use this for initialization
-    void Start () {
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetButtonDown("Jump") && moving)
-        {
-            goTo(followTarget);
-            moving = !moving;
-        }
-        else if(Input.GetButtonDown("Jump") && !moving)
-        {
-            stop();
-            moving = !moving;
-        }
-	}
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetButtonDown("Jump") && moving)
+        //{
+        //    goTo(followTarget);
+        //    moving = !moving;
+        //}
+        //else if (Input.GetButtonDown("Jump") && !moving)
+        //{
+        //    stop();
+        //    moving = !moving;
+        //}
+    }
 
 
     // go to function
-    void goTo ( Transform target )
+    public void goTo(Transform target)
     {
         GetComponentInParent<AILerp>().target = target;
     }
-    void stop ()
+    void stop()
     {
         GetComponentInParent<AILerp>().target = null;
+    }
+
+    public void OnRaycastHit()
+    {
+        print("player was hit");
+    }
+
+    //used by data structures like HashSet, List and etc . To find an element/check if it exists there.
+    public bool Equals(PotatoBasicAI other)
+    {
+        return this.transform.Equals(other);
     }
 }
---
+++ Potatoes/Assets/Scripts/GameManager.cs
@@ -0,0 +1,94 @@
+﻿using Assets.Scripts.Interfaces;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public delegate void ChangeTarget(Transform target);
+    private event ChangeTarget OnTargetChange;
+    private HashSet<GameObject> chosenAIList;
+
+    // Use this for initialization
+    void Start()
+    {
+        chosenAIList = new HashSet<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //left mouse button down
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+            RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 0f);
+
+            if (hit)
+            {
+                GameObject hitedGameObj = hit.collider.gameObject;
+                //search for IPotatoAI component in hited gameObject and all its children
+                var pbAI = hitedGameObj.GetComponentInChildren<IPotatoAI>();
+                //if component exists register its function goTo to OnTargetChange
+                if (pbAI != null)
+                {
+                    //if that was first click on AI add it to data
+                    if (!chosenAIList.Contains(hitedGameObj))
+                        AddAIPlayer(hitedGameObj, pbAI);
+                    //if player clicked on AI second time - remove it from data.
+                    else
+                        RemoveAIPlayer(hitedGameObj, pbAI);
+                }
+            }
+            //if user clicked on empty space remove all selected AIs
+            else
+            {
+                chosenAIList.Clear();
+                if(OnTargetChange != null)
+                {
+                    //Set target to null
+                    OnTargetChange(null);
+                    //Remove all registered functions
+                    OnTargetChange = null;
+
+                }
+            }
+
+        }
+        //right mouse button down
+        if (Input.GetMouseButtonDown(1))
+        {
+            Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+            RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 0f);
+
+            if (hit)
+            {
+                //if there functions registered for this event then raise it
+                if (OnTargetChange != null)
+                    OnTargetChange(hit.transform);
+            }
+            //if user clicked on object without collider 
+            else
+            {
+                //TODO
+                //create transform data from mouse click location
+                // Transform position = new Transform();
+
+            }
+        }
+
+    }
+
+    private void AddAIPlayer(GameObject hitedGameObj, IPotatoAI pbAI)
+    {
+        chosenAIList.Add(hitedGameObj);
+        OnTargetChange += pbAI.goTo;
+    }
+
+    private void RemoveAIPlayer(GameObject hitedGameObj, IPotatoAI pbAI)
+    {
+        chosenAIList.Remove(hitedGameObj);
+        OnTargetChange -= pbAI.goTo;
+    }
+}
